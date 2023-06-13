3a8af12d03ebb4964a6e7182a2c0f03ad21fd306
Schlagonia <schlagonia@gmail.com>
2023-06-13 11:53:03 -0600

    chore: deployments

---
+++ contracts/Mocks/Deployer.sol
@@ -0,0 +1,9 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Deployer {
+    event Deployed(address addr, uint256 salt);
+
+    function deploy(bytes memory code, uint256 salt) external {}
+}
