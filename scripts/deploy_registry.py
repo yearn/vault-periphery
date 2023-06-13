34c608ad95e004e63a653ab9381c54b9cd2c0c19
Schlagonia <schlagonia@gmail.com>
2023-06-13 11:54:30 -0600

    fix: black

--- scripts/deploy_registry.py
+++ scripts/deploy_registry.py
@@ -19,14 +19,16 @@ def deploy_release_and_factory():
     release_registry = project.ReleaseRegistry
     factory = project.RegistryFactory
     deployer = accounts.load("v3_deployer")
-    deployer_contract = project.Deployer.at("0x488E1A80133870CB71EE2b08f926CE329d56B084")
+    deployer_contract = project.Deployer.at(
+        "0x488E1A80133870CB71EE2b08f926CE329d56B084"
+    )
 
     salt_string = "v3.0.1-beta"
 
     # Create a SHA-256 hash object
     hash_object = hashlib.sha256()
     # Update the hash object with the string data
-    hash_object.update(salt_string.encode('utf-8'))
+    hash_object.update(salt_string.encode("utf-8"))
     # Get the hexadecimal representation of the hash
     hex_hash = hash_object.hexdigest()
     # Convert the hexadecimal hash to an integer
@@ -34,20 +36,23 @@ def deploy_release_and_factory():
 
     print(f"Salt we are using {salt}")
     print("Init balance:", deployer.balance / 1e18)
-    
+
     # generate and deploy release registry
     release_constructor = release_registry.constructor.encode_input("GOV")
 
     release_deploy_bytecode = HexBytes(
-        HexBytes(release_registry.contract_type.deployment_bytecode.bytecode) + release_constructor
-        )
-    
+        HexBytes(release_registry.contract_type.deployment_bytecode.bytecode)
+        + release_constructor
+    )
+
     print(f"Deploying Release Registry...")
 
-    release_tx = deployer_contract.deploy(release_deploy_bytecode, salt, sender=deployer)
+    release_tx = deployer_contract.deploy(
+        release_deploy_bytecode, salt, sender=deployer
+    )
 
     release_event = list(release_tx.decode_logs(deployer_contract.Deployed))
-    
+
     release_address = release_event[0].addr
 
     print(f"Deployed the vault release to {release_address}")
@@ -58,17 +63,20 @@ def deploy_release_and_factory():
     factory_constructor = factory.constructor.encode_input(release_address)
 
     factory_deploy_bytecode = HexBytes(
-        HexBytes(factory.contract_type.deployment_bytecode.bytecode) + factory_constructor
-        )
-    
-    factory_tx = deployer_contract.deploy(factory_deploy_bytecode, salt, sender=deployer)
+        HexBytes(factory.contract_type.deployment_bytecode.bytecode)
+        + factory_constructor
+    )
+
+    factory_tx = deployer_contract.deploy(
+        factory_deploy_bytecode, salt, sender=deployer
+    )
 
     factory_event = list(factory_tx.decode_logs(deployer_contract.Deployed))
 
     deployed_factory = factory.at(factory_event[0].addr)
 
     print(f"Deployed Rgistry Factory to {deployed_factory.address}")
-    
+
 
 def main():
     deploy_release_and_factory()
