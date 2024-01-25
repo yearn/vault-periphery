4c52060f2268a415e1fe89c560e8177b3f4e2431
Schlagonia <schlagonia@gmail.com>
2024-01-24 19:47:06 -0700

    fix: apr oracle governance

--- contracts/Mocks/MockOracle.sol
+++ contracts/Mocks/MockOracle.sol
@@ -3,4 +3,6 @@ pragma solidity 0.8.18;
 
 import {AprOracle} from "@periphery/AprOracle/AprOracle.sol";
 
-contract MockOracle is AprOracle {}
+contract MockOracle is AprOracle {
+    constructor() AprOracle(msg.sender) {}
+}
